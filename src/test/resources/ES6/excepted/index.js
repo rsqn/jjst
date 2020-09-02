2fdebe72cb9ceb250cf53eb1a251b216a5410310
Andy Chau <>
2020-09-02 16:43:24 +1000

    Using Closure and json object with class functions include constructor.

--- src/test/resources/ES6/excepted/index.js
+++ src/test/resources/ES6/excepted/index.js
@@ -31,39 +31,72 @@ mr.register('./js/tools.js', {
     }
 });
 
+let UserFn = function() {
 
-mr.register('./js/user.js',
-    class User {
-        constructor(firstName, lastName) {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.visits = 0;
+    let firstName, lastName;
+    let visits;
+
+    return {
+        User: function(_firstName, _lastName) {
+            firstName = _firstName;
+            lastName = _lastName;
+            visits = 0;
+        }, 
+        fullName: function() {
+            return `${firstName} ${lastName}`;
+        },
+        doubleAge: function () {
+            return mr.get('./tools.js').doubleIt(visits);
+        },
+        visiting: function() {
+            return visits += 1;
         }
+    };
+}
 
-        fullName() {
-            return `${this.firstName} ${this.lastName}`;
-        }
+console.log('UserFn: ' + UserFn);
 
-        doubleAge() {
-            return mr.get('./tools.js').doubleIt(this.visits);
-        }
+mr.register('./js/user.js', UserFn);
 
-        visiting() {
-            return this.visits += 1;
-        }
-    }
-)
+// emulate the User class
+let UserClass = mr.get('./js/user.js');
 
-// If the export is a class than has to assign to variable in order to perform the new operation.
-let User = mr.get('./js/user.js')
+console.log('');
+console.log('====== user 1 ========');
+// emulate the new instance
+let user1 = UserClass();
+user1.User('James', 'Bond');
 
-let jb = new User('James', 'Bond');
+console.log(`user1.firstName: ${user1.firstName}`);
+console.log(`user1.fullName: ${user1.fullName()}`);
+console.log(`user1.visiting: ${user1.visiting()}`);
+console.log(`user1.visiting: ${user1.visiting()}`);
 
-console.log(jb.fullName());
-console.log(jb.visiting());
-console.log(jb.visiting());
-console.log(jb.doubleAge())
+console.log('');
+console.log('====== user 2 ========');
+
+let user2 = UserClass();
+user2.User('Second', 'User');
+
+console.log(`user2.fullName: ${user2.fullName()}`);
+console.log(`user2.visiting: ${user2.visiting()}`);
+console.log(`user2.visiting: ${user2.visiting()}`);
+console.log(`user2.visiting: ${user2.visiting()}`);
+console.log(`user2.visiting: ${user2.visiting()}`);
+
+console.log('');
+console.log('====== reaccess ========');
+
+// user1
+console.log(`user1.fullName: ${user1.fullName()}`);
+console.log(`user1.doubleAge: ${user1.doubleAge()}`);
+
+// user2
+console.log(`user2.fullName: ${user2.fullName()}`);
+console.log(`user2.doubleAge: ${user2.doubleAge()}`);
 
 
 // If it is not a class export will return function directly
+console.log('');
+console.log('====== tools.js ========');
 console.log(mr.get('./js/tools.js').doubleIt(2));
